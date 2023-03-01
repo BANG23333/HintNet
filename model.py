3957d5631a8c7a497bdceb7ab52b0965ea44630c
BANG23333 <an1328910990@gmail.com>
2023-03-01 09:42:06 -0600

    Update model.py

--- model.py
+++ model.py
@@ -41,16 +41,7 @@ class NeuralNet(nn.Module):
             # cnn_list3.append(GraphConvolution(num_spatial_tempoal, num_spatial_tempoal))
 
         self.cnn3d = nn.ModuleList(cnn_list)
-        # self.cnn3d_2 = nn.ModuleList(cnn_list2)
-        # self.cnn3d_3 = nn.ModuleList(cnn_list3)
-
-        # self.bn1 = nn.BatchNorm2d(15)
-        # self.bn2 = nn.BatchNorm2d(15)
-
-        # self.three_d_cnn = nn.Conv3d(in_channels=num_spatial_tempoal, out_channels=num_spatial_tempoal, kernel_size = (1, S2, S2), padding="same")
-        # self.two_d_cnn = nn.Conv2d(in_channels=num_spatial, out_channels=num_spatial, kernel_size = (S2, S2), padding="same")
         self.two_d_cnn = GraphConv(num_spatial, num_spatial)
-        # self.two_d_cnn2 = GraphConvolution(num_spatial, num_spatial)
 
         self.LSTM = nn.LSTM((num_temporal + num_spatial_tempoal), hidden_size=num_temporal + num_spatial_tempoal,
                             batch_first=True)
@@ -66,36 +57,15 @@ class NeuralNet(nn.Module):
         spatial_tempoal_view = x[:, :, :, :,
                                self.num_temporal + self.num_spatial:self.num_temporal + self.num_spatial + self.num_spatial_tempoal]
 
-        # spatial_tempoal_view = spatial_tempoal_view.permute(0, 4, 1, 2, 3)
-        # spatial_tempoal_view = self.three_d_cnn(spatial_tempoal_view).
-
         for i in range(7):
-            # spatial_tempoal_view[:, i] = self.cnn3d[i](spatial_tempoal_view[:, i], torch.zeros((x.shape[0], 25, 25), dtype=torch.float32, device=torch.device('cuda:0')))
             spatial_tempoal_view[:, i] = self.cnn3d[i](spatial_tempoal_view[:, i], adj)
-            # spatial_tempoal_view[:, i] = F.relu(spatial_tempoal_view[:, i])
-            # spatial_tempoal_view[:, i] = self.bn1(spatial_tempoal_view[:, i])
-            # spatial_tempoal_view[:, i] = self.cnn3d_2[i](spatial_tempoal_view[:, i], adj)
-            # spatial_tempoal_view[:, i] = F.relu(spatial_tempoal_view[:, i])
-            # spatial_tempoal_view[:, i] = self.cnn3d_3[i](spatial_tempoal_view[:, i], adj)
-            # spatial_tempoal_view[:, i] = F.relu(spatial_tempoal_view[:, i])
-            # spatial_tempoal_view[:, i] = self.bn2(spatial_tempoal_view[:, i])
 
         spatial_tempoal_view = F.relu(spatial_tempoal_view)
-
-        # spatial_tempoal_view = spatial_tempoal_view.permute(0, 2, 3, 4, 1)
-
-        # spatial_view = spatial_view.permute(0, 3, 1, 2)
-        # spatial_view = self.two_d_cnn(spatial_view, torch.zeros((x.shape[0], 25, 25), dtype=torch.float32, device=torch.device('cuda:0')))
         spatial_view = self.two_d_cnn(spatial_view, adj)
         spatial_view = F.relu(spatial_view)
-        # spatial_view = self.two_d_cnn2(spatial_view, adj)
-        # spatial_view = F.relu(spatial_view)
-
-        # spatial_view = spatial_view.permute(0, 2, 3, 1)
 
         spatial_tempoal_view = self.FC(spatial_tempoal_view.flatten(1))
         spatial_tempoal_view = torch.reshape(spatial_tempoal_view, (len(x), self.input_len, self.num_spatial_tempoal))
-
         merged_two_view = torch.cat((spatial_tempoal_view, temporal_view), 2)
 
         current, (h_n, c_n) = self.LSTM(merged_two_view)
